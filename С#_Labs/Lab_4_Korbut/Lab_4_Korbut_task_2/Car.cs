35a91a4fd744e61bdf577616273f6c4fb37f60ca
nebiros88 <36847332+nebiros88@users.noreply.github.com>
2019-07-29 13:00:39 +0300

    new

--- С#_Labs/Lab_4_Korbut/Lab_4_Korbut_task_2/Car.cs
+++ С#_Labs/Lab_4_Korbut/Lab_4_Korbut_task_2/Car.cs
@@ -42,59 +42,37 @@ namespace Lab_4_Korbut_task_2
         // Метод записи информации о классе в текстовый файл имя которого передается параметром
         public static void WriteInfoFile(string fileName, Type t)
         {
-<<<<<<< HEAD
             string newFileFolder = @"D:\BSUIR\Directory_for_lab_4/" + fileName + ".txt";
-=======
-            string newFileFolder = @"C:\Users\Korbut\Documents\BSUIR\Корбут\For_lab_4_C#\" + fileName + ".txt";
-            //FileInfo infoFile = new FileInfo(newFileFolder);
-            File.Create(newFileFolder);
->>>>>>> 781a3b03a60d301778708f829cb2db6f1acfce6a
             try
             {
                 using (StreamWriter sw = new StreamWriter(newFileFolder, true, System.Text.Encoding.Default))
                 {
                     sw.Write("Полное имя класса - " + t.FullName);
-<<<<<<< HEAD
                     sw.Write("\n");
-=======
->>>>>>> 781a3b03a60d301778708f829cb2db6f1acfce6a
                     if (t.IsAbstract) sw.Write("Абстрактный класс");
                     if (t.IsClass) sw.Write("Обычный класс");
                     if (t.IsEnum) sw.Write("Перечисление");
                     if (t.IsInterface) sw.Write("Интерфейс");
                     if (t.IsPrimitive) sw.Write("Примитив");
                     if (t.IsSealed) sw.Write("Закрыт для наледования");
-<<<<<<< HEAD
                     sw.Write("\n");
                     sw.Write("Базовый класс - " + t.BaseType);
                     sw.Write("\n");
                     sw.Write("++++++++ ПОЛЯ КЛАССА +++++++++");
                     sw.Write("\n");
-=======
-                    sw.Write("Базовый класс - " + t.BaseType);
-                    sw.Write("++++++++ ПОЛЯ КЛАССА +++++++++");
->>>>>>> 781a3b03a60d301778708f829cb2db6f1acfce6a
                     FieldInfo[] m = t.GetFields();
                     foreach (FieldInfo x in m)
                     {
                         sw.Write(x);
-<<<<<<< HEAD
                         sw.Write("\n");
                     }
                     sw.Write("+++++++ СВОЙСТВА КЛАССА +++++++++");
                     sw.Write("\n");
-=======
-                    }
-                    sw.Write("+++++++ СВОЙСТВА КЛАССА +++++++++");
->>>>>>> 781a3b03a60d301778708f829cb2db6f1acfce6a
                     PropertyInfo[] m1 = t.GetProperties();
                     foreach (PropertyInfo x in m1)
                     {
                         sw.Write(x);
-<<<<<<< HEAD
                         sw.Write("\n");
-=======
->>>>>>> 781a3b03a60d301778708f829cb2db6f1acfce6a
                     }
                     sw.Close();
                 }
