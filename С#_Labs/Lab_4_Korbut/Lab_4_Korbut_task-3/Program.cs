2694043f6f03d7717a6de841901456fb7418b937
nebiros88 <36847332+nebiros88@users.noreply.github.com>
2019-08-01 16:22:35 +0300

    friday new

---
+++ С#_Labs/Lab_4_Korbut/Lab_4_Korbut_task-3/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_4_Korbut_task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                int choice = 0;
+                Console.WriteLine("+++ Выберите действие +++");
+                Console.WriteLine("1 - ввод строки с клавиатуры");
+                Console.WriteLine("2 - вывод строки");
+                Console.WriteLine("3 - после указанного символа каждый раз вставить *");
+                Console.WriteLine("4 - заменить один символ на другой");
+                Console.WriteLine("5 - удалить все вхождения указанной подстроки");
+                Console.WriteLine("0 - выход");
+                choice = int.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    default: return;
+                }
+                if (choice < 0 || choice > 5)
+                {
+                    Console.WriteLine("Сделайте правильный выбор!");
+                    continue;
+                }
+                else if (choice > 0 && choice < 6)
+                {
+                    continue;
+                }
+                else break;
+            }
+        }
+    }
+}
