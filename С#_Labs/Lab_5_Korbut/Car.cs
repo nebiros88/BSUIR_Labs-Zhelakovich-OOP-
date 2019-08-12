c3a42ca62c0b5e9e7a6804503e8d0fb99950512c
nebiros88 <36847332+nebiros88@users.noreply.github.com>
2019-08-12 07:57:18 +0300

    new

--- С#_Labs/Lab_5_Korbut/Car.cs
+++ С#_Labs/Lab_5_Korbut/Car.cs
@@ -13,6 +13,7 @@ namespace Lab_5_Korbut
         public int currentSpeed = 0;
         public bool engineState = true;
         private int limitSpeed = 300;
+        public List<int> SpeedValues = new List<int>();
         public int MaxSpeed
         {
             get { return maxSpeed; }
@@ -41,31 +42,55 @@ namespace Lab_5_Korbut
             this.maxSpeed = _maxSpeed;
         }
 
-        public delegate void CarStateHandler(string message);   // Обьявление делегата
+        public delegate void CarStateHandler(string message);   // Обьявление делегата (для события без параметра)
+        public delegate void LimitTrafficSpeed(int n);          // -//- (для события с параметром)
+        public delegate int SpeedHandler(List<int> nameList);
         public event CarStateHandler DamageEngine;              // Событие возникающее при поломке двигателя
+        public event LimitTrafficSpeed Warn;                    // Событие предупреждающее о достижении ограничения скорости
+        public event SpeedHandler AverageSpeed;
 
-        public static bool CheckEngine(Car _car)
+        private bool CheckEngine()
         {
-            if (_car.maxSpeed - _car.currentSpeed > 10)  Console.WriteLine("\nАвтомобиль продолжает движение!");
-            if (_car.maxSpeed - _car.currentSpeed <= 10) Console.WriteLine("\nАвтомобиль продолжает движение! Высокая скорость! ");
-            if (_car.currentSpeed == _car.maxSpeed) Console.WriteLine("\nДостигнута максимальная скорость! Двигатель выйдет из строя!");
-            if (_car.currentSpeed > _car.maxSpeed) _car.engineState = false;
-            return _car.engineState;
+            if (maxSpeed - currentSpeed > 10)  Console.WriteLine("\nАвтомобиль продолжает движение!");
+            if (maxSpeed - currentSpeed <= 10) Console.WriteLine("\nАвтомобиль продолжает движение! Высокая скорость! ");
+            if (currentSpeed == maxSpeed) Console.WriteLine("\nДостигнута максимальная скорость! Двигатель выйдет из строя!");
+            if (currentSpeed > maxSpeed) engineState = false;
+            return engineState;
         }
         public void Accelerate( int acceleration)
         {
             for ( ; engineState != false; currentSpeed += acceleration)
             {
-                CheckEngine(this);
+                CheckEngine();
                 Console.WriteLine("Автомобиль с именем {0} ускорился на {1}км/ч. Текущая скорость автомобиля составляет - {2}км/ч", name, acceleration, currentSpeed);
             }
             if (!engineState)                         // Условие для возникновения события (Событие без параметра)
             {
                 if (DamageEngine != null)                       
                 {
-                    DamageEngine($"Двигатель поврежден!");
+                    DamageEngine("Двигатель поврежден!");
                 }
             }
+        }
+        public void AccelerateForMax(int n)
+        {
+            for (currentSpeed = 10; currentSpeed <= maxSpeed; currentSpeed += 10)
+            {
+                Console.WriteLine($"Машина с именем -{name} - ускорилась до {currentSpeed} км/ч.");
+                SpeedValues.Add(currentSpeed);
+                if (currentSpeed == n)                  // Условие для возникновения события с параметром
+                {
+                    Warn?.Invoke(currentSpeed);
+                }
+                if (currentSpeed == maxSpeed)
+                {
+                    AverageSpeed?.Invoke(SpeedValues);
+                }
+            }
+        }
+        public void ShowMessage(int n)
+        {
+            Console.WriteLine($"Вы превысили ограниечение скорости для данного участка (ограничение - {n} км/ч).");
         }
     }
 }
--- С#_Labs/Lab_5_Korbut/Program.cs
+++ С#_Labs/Lab_5_Korbut/Program.cs
@@ -8,14 +8,35 @@ namespace Lab_5_Korbut
 {
     class Program
     {
+        delegate int SummFine(int x, int y);
+
         static void Main(string[] args)
         {
+            SummFine fineGet = (x, y) => (x + y);
             Car newCar = new Car("Ford", 180);
             newCar.DamageEngine += ShowMessage;
+            newCar.Warn += newCar.ShowMessage;
+            newCar.Warn += delegate (int n) { Console.WriteLine($"При скорости более {n} км/ч вы получите штраф!");};   // Анонимный делегат
             newCar.Accelerate(10);
+            Console.WriteLine();
+            newCar.AccelerateForMax(90);
+            Console.WriteLine();
+            Car raceCar = new Car("BMW", 200);
+            raceCar.AverageSpeed += speedList => 
+            {
+                int sum = 0;
+                int result = 0;
+                foreach (int val in raceCar.SpeedValues)
+                {
+                    sum += val;
+                }
+                result = sum / speedList.Capacity;
+                Console.WriteLine($"Средняя скорость за ускорение машины с именем {raceCar.name} составила {result} км/ч");
+                return result;
+            };
+            raceCar.AccelerateForMax(90);
         }
-
-        public static void ShowMessage(string message)
+        public static void ShowMessage(string message)      //Статический метод любого класса
         {
             Console.WriteLine(message);
         }
