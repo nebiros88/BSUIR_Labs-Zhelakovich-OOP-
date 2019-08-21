af2aba034c0bd7dae89e6a6958dd7bb838fe1487
nebiros88 <36847332+nebiros88@users.noreply.github.com>
2019-08-21 15:16:47 +0300

    new

---
+++ С#_Labs/Lab_6_Korbut/Lab_6_Korbut_task_5/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Lab_6_Korbut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Values values1 = new Values(0, 20);
+            Values values2 = new Values(20, 40);
+            Task<string> task1 = Task<string>.Factory.StartNew(() => GetStarString(5));
+            Console.WriteLine(task1.Result);
+            var outer = Task.Factory.StartNew(() =>
+            {
+                values1.GetOddNumbers();
+                var inner = Task.Factory.StartNew(() =>
+                {
+                    values2.GetEvenNumbers();
+                },TaskCreationOptions.AttachedToParent);        //Выполнение вложенной задачи вместе с внешней
+            });
+            outer.Wait();
+        }
+
+        static string GetStarString(object x)
+        {
+            for (int i = 1; i <= (int)x; i++)
+            {
+               Console.WriteLine(Stars(i));
+            }
+            for (int i = (int)x - 1; i > 0; i--)
+            {
+                Console.WriteLine(Stars(i));
+            }
+            return "Конец 3-го метода";
+        }
+
+        static string Stars(int size)
+        {
+            return new string('*', size);
+        }
+    }
+}
---
+++ С#_Labs/Lab_6_Korbut/Lab_6_Korbut_task_5/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Lab_6_Korbut_task_5")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lab_6_Korbut_task_5")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("a810d2e2-f921-435f-a9ca-b40ae061bf65")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
